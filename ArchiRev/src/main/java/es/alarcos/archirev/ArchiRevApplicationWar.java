b839698763cc25ac9ce44ef0cc4f573beefe9a2a
Ricardo <ricardo.pdelcastillo@uclm.es>
2018-06-20 19:30:16 +0200

    Add awesome fonts

--- ArchiRev/src/main/java/es/alarcos/archirev/ArchiRevApplicationWar.java
+++ ArchiRev/src/main/java/es/alarcos/archirev/ArchiRevApplicationWar.java
@@ -1,10 +1,15 @@
 
 package es.alarcos.archirev;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ArchiRevApplicationWar extends SpringBootServletInitializer {
@@ -16,5 +21,16 @@ public class ArchiRevApplicationWar extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(ArchiRevApplicationWar.class);
+	}
+	
+	@Bean
+	public ServletContextInitializer servletContextCustomizer() {
+		return new ServletContextInitializer() {
+			@Override
+			public void onStartup(ServletContext servletContext) throws ServletException {
+				servletContext.setInitParameter("primefaces.FONT_AWESOME", Boolean.TRUE.toString());
+				servletContext.setInitParameter("primefaces.THEME", "bootstrap");
+			}
+		};
 	}
 }
