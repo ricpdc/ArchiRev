7373718ad20f7173d885cf25da6f40a80d51564e
Ricardo <ricardo.pdelcastillo@uclm.es>
2018-10-15 16:48:37 +0200

    Fix CSharp parser for recognizing class names

--- ArchiRev/src/test/java/es/alarcos/archirev/CsharpParserTest.java
+++ ArchiRev/src/test/java/es/alarcos/archirev/CsharpParserTest.java
@@ -1,7 +1,10 @@
 package es.alarcos.archirev;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -15,19 +18,24 @@ import org.junit.jupiter.api.Test;
 
 import es.alarcos.archirev.parser.csharp.CSharpLexer;
 import es.alarcos.archirev.parser.csharp.CSharpParser;
+import es.alarcos.archirev.parser.csharp.CSharpParser.Class_definitionContext;
 import es.alarcos.archirev.parser.csharp.CSharpParser.Compilation_unitContext;
-import es.alarcos.archirev.parser.csharp.CSharpParser.Type_declarationContext;
-
-
+import es.alarcos.archirev.parser.csharp.CSharpParser.Delegate_definitionContext;
+import es.alarcos.archirev.parser.csharp.CSharpParser.Enum_definitionContext;
+import es.alarcos.archirev.parser.csharp.CSharpParser.Interface_definitionContext;
+import es.alarcos.archirev.parser.csharp.CSharpParser.Struct_definitionContext;
 
 @SuppressWarnings("deprecation")
 class CsharpParserTest {
 
-	private static final String C_SHARP_FILE_PATH = "C:\\Users\\Alarcos\\git\\ArchiRev\\ArchiRev\\src\\test\\resources\\csharp_sources\\test.cs";
 	private static final String TEST_FOLDER = "D:\\Universidad\\CASOS DE ESTUDIO\\ArquiRev\\bari";
 	private int numberOfCsharpFiles;
-	private int wrongFiles;
-	private Set<String> attributes = new TreeSet<String>();
+	private List<String> wrongFiles;
+	private Set<String> classes = new TreeSet<String>();
+	private Set<String> enums = new TreeSet<String>();
+	private Set<String> interfaces = new TreeSet<String>();
+	private Set<String> structs = new TreeSet<String>();
+	private Set<String> delegates = new TreeSet<String>();
 
 	@Test
 	void test() throws IOException {
@@ -35,19 +43,42 @@ class CsharpParserTest {
 		// for (SyntaxError syntaxError : listener.getSyntaxErrors()) {
 		// System.out.println(syntaxError.toString());
 		// }
-		
-		
+
 		numberOfCsharpFiles = 0;
-		wrongFiles = 0;
+		wrongFiles = new ArrayList<>();
 		parseCsharpFolder(TEST_FOLDER);
 		System.out.println("\n\n\nAnlyzed files: " + numberOfCsharpFiles);
-		System.out.println("\n\n\nWrong files: " + wrongFiles);
-		
-		
-		System.out.println("\n\n\nDifferent unique attributes:\n");
-		for (String attribute: attributes) {
-			System.out.println(attribute);
+		System.out.println("\n\n\nWrong files: " + wrongFiles.size());
+
+		for (String fileName : wrongFiles) {
+			System.out.println(fileName);
 		}
+
+		System.out.println("\n\n\n" + classes.size() + " Different unique classes:\n");
+		for (String clazz : classes) {
+			System.out.println(clazz);
+		}
+
+		System.out.println("\n\n\n" + enums.size() + " Different unique enums:\n");
+		for (String en : enums) {
+			System.out.println(en);
+		}
+
+		System.out.println("\n\n\n" + interfaces.size() + " Different unique interfaces:\n");
+		for (String inter : interfaces) {
+			System.out.println(inter);
+		}
+
+		System.out.println("\n\n\n" + structs.size() + " Different unique structs:\n");
+		for (String st : structs) {
+			System.out.println(st);
+		}
+
+		System.out.println("\n\n\n" + delegates.size() + " Different unique delegates:\n");
+		for (String del : delegates) {
+			System.out.println(del);
+		}
+
 	}
 
 	private void parseCsharpFolder(String path) {
@@ -65,7 +96,6 @@ class CsharpParserTest {
 					numberOfCsharpFiles++;
 				} catch (Exception ex) {
 					ex.printStackTrace();
-					wrongFiles++;
 				}
 			}
 		}
@@ -74,7 +104,24 @@ class CsharpParserTest {
 	private void parserCsharpFile(File file) throws IOException {
 		// System.out.println("Parsing file:" + file.getAbsolutePath());
 
-		ANTLRFileStream input = new ANTLRFileStream(file.getAbsolutePath());
+		// create temp file without directives
+		File tempFile = File.createTempFile(file.getAbsolutePath(), "-withoutDirectives.tmp");
+		FileWriter fw = new FileWriter(tempFile);
+
+		List<String> lines = Files.readAllLines(file.toPath(), Charset.defaultCharset());
+		for (String line : lines) {
+			if (!line.trim().startsWith("#") && !line.trim().startsWith("﻿#")) {
+				fw.write(line + "\n");
+			}
+		}
+
+		fw.close();
+
+		if (Files.readAllBytes(tempFile.toPath()).length == 0) {
+			return;
+		}
+
+		ANTLRFileStream input = new ANTLRFileStream(tempFile.getAbsolutePath());
 		CSharpLexer lexer = new CSharpLexer(input);
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
 		CSharpParser parser = new CSharpParser(tokens);
@@ -82,63 +129,38 @@ class CsharpParserTest {
 		SyntaxErrorListener listener = new SyntaxErrorListener();
 		parser.addErrorListener(listener);
 
-		List<Object> elements = new ArrayList<>();
 		Compilation_unitContext compilation_unit = parser.compilation_unit();
 		for (int i = 0; i < compilation_unit.getChildCount(); i++) {
-			ParseTree child = compilation_unit.getChild(i);
-			elements.addAll(getElements(child));
+			ParseTree tree = compilation_unit.getChild(i);
+			getElements(tree);
 		}
 
-		CSharpClassVisitor visitor = new CSharpClassVisitor();
-		// List<String> result =
-		// visitor.visitClass_member_declarations(parser.class_member_declarations());
-		List<String> result = visitor
-				.visitNamespace_member_declarations(parser.compilation_unit().namespace_member_declarations());
+		System.out.println("Parsing.... " + file.getAbsolutePath());
+		if (!listener.getSyntaxErrors().isEmpty()) {
+			wrongFiles.add(file.getAbsolutePath());
+		}
+		for (SyntaxError syntaxError : listener.getSyntaxErrors()) {
+			System.out.println("\tERROR: " + syntaxError);
+		}
 
-		//System.out.println(result);
 	}
 
-	public List<Object> getElements(ParseTree child) {
-		List<Object> res = new ArrayList<Object>();
-		if (child.getChildCount() == 0) {
-			// base case
-
-		}
-		if (child.getChildCount() == 1) {
-			ParseTree nestedChild = child.getChild(0);
-			if (nestedChild.getClass().equals(Type_declarationContext.class)) {
-				getElements(nestedChild);
-				res.add(nestedChild);
-				System.out.println("\n"+nestedChild.getText());
-				Type_declarationContext type_declarationContext = ((Type_declarationContext) nestedChild);
-				if (type_declarationContext.class_definition() != null) {
-					System.out.println(
-							"\n" + type_declarationContext.class_definition().identifier().IDENTIFIER().getText());
-					attributes.add(type_declarationContext.class_definition().identifier().IDENTIFIER().getText());
-				}
-			} 
-//				else if (nestedChild.getClass().equals(AttributeContext.class)) {
-//				getElements(nestedChild);
-//				res.add(nestedChild);
-//				// System.out.println("\n"+nestedChild.getText());
-//				AttributeContext attributeContext = ((AttributeContext) nestedChild);
-//				System.out.println("\n" + attributeContext.getText() + ":"
-//						+ (attributeContext.attribute_argument().isEmpty() ? " "
-//								: attributeContext.attribute_argument(0).getText()));
-//				attributes.add(attributeContext.getText());
-//			}
-
-			
-			// System.out.println("\n"+nestedChild.getClass().getSimpleName() + " --> " +
-			// nestedChild.getText());
-
-		} else {
-			for (int i = 0; i < child.getChildCount(); i++) {
-				ParseTree nestedChild = child.getChild(i);
-				res.addAll(getElements(nestedChild));
+	public void getElements(ParseTree tree) {
+		for (int i = 0; i < tree.getChildCount(); i++) {
+			ParseTree subTree = tree.getChild(i);
+			if (subTree.getClass().equals(Class_definitionContext.class)) {
+				classes.add(((Class_definitionContext) subTree).identifier().IDENTIFIER().getText());
+			} else if (subTree.getClass().equals(Enum_definitionContext.class)) {
+				enums.add(((Enum_definitionContext) subTree).identifier().IDENTIFIER().getText());
+			} else if (subTree.getClass().equals(Interface_definitionContext.class)) {
+				interfaces.add(((Interface_definitionContext) subTree).identifier().IDENTIFIER().getText());
+			} else if (subTree.getClass().equals(Struct_definitionContext.class)) {
+				structs.add(((Struct_definitionContext) subTree).identifier().IDENTIFIER().getText());
+			} else if (subTree.getClass().equals(Delegate_definitionContext.class)) {
+				delegates.add(((Delegate_definitionContext) subTree).identifier().IDENTIFIER().getText());
 			}
+			getElements(subTree);
 		}
-		return res;
 	}
 
 }
