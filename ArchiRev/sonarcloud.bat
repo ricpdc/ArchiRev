6c83e87633266c76c4e9e7882fbc58df69d3bfc3
Ricardo <ricardo.pdelcastillo@uclm.es>
2018-12-18 11:27:57 +0100

    sonarcloud.bat

---
+++ ArchiRev/sonarcloud.bat
@@ -0,0 +1,3 @@
+cd C:\Users\Alarcos\git\ArchiRev\ArchiRev
+
+mvn sonar:sonar   -Dsonar.projectKey=ricpdc_ArchiRev  -Dsonar.organization=ricpdc-github   -Dsonar.host.url=https://sonarcloud.io   -Dsonar.login=cbd8917894a7051aab9efabb9f32e39b9944e7b9
